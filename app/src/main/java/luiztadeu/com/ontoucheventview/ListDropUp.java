20d4e8d37cef2f971d726fc11fd1c7888f5f0cda
x181868 <luiz.mendes@resource.com.br>
2017-01-10 19:01:50 -0200

    ajuste animaçao

--- app/src/main/java/luiztadeu/com/ontoucheventview/ListDropUp.java
+++ app/src/main/java/luiztadeu/com/ontoucheventview/ListDropUp.java
@@ -4,15 +4,16 @@ package luiztadeu.com.ontoucheventview;
  * Created by Shark on 26/12/2016.
  */
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -24,7 +25,6 @@ public class ListDropUp extends RelativeLayout implements View.OnTouchListener {
     private LinearLayout linearLayoutMove;
     private float dX, dY;
     private int position = dpToPx(460);
-    int lastAction;
     boolean isUp = false;
     private ListAdapter adapter;
     private DisplayMetrics displayMetrics;
@@ -44,14 +44,14 @@ public class ListDropUp extends RelativeLayout implements View.OnTouchListener {
         super(context, attrs, defStyleAttr);
     }
 
-    public void init(Activity mActivity){
+    public void init(final Activity mActivity) {
 
-        View view = LayoutInflater.from(mActivity).inflate(R.layout.activity_main, this);
-        rootLayout = (LinearLayout) view.findViewById(R.id.root_layout);
-        linearLayoutMove = (LinearLayout) view.findViewById(R.id.linear_layout);
+        final View view = LayoutInflater.from(mActivity).inflate(R.layout.activity_main, this);
+        //rootLayout = (LinearLayout) view.findViewById(R.id.root_linear);
+//        linearLayoutMove = (LinearLayout) view.findViewById(R.id.linear_layout);
         listView = (ListView) view.findViewById(R.id.listview1);
         displayMetrics = getResources().getDisplayMetrics();
-        listView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));//(displayMetrics.heightPixels * 40) / 100));
+        //listView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));//(displayMetrics.heightPixels * 40) / 100));
 
         stringList = new ArrayList<>();
         stringList.add("1");
@@ -73,52 +73,115 @@ public class ListDropUp extends RelativeLayout implements View.OnTouchListener {
 
         adapter = new ListAdapter(mActivity, stringList);
         listView.setAdapter(adapter);
-        linearLayoutMove.setOnTouchListener(this);
-        linearLayoutMove.setY(displayMetrics.heightPixels - (displayMetrics.heightPixels - position));
+        //rootLayout.setOnTouchListener(this);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+
+                if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_FLING) {
+                    listView.setOnTouchListener(new OnTouchListener() {
+
+                        @Override
+                        public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                            switch (motionEvent.getActionMasked()) {
+                                case MotionEvent.ACTION_UP:
+
+                                    if (!isUp) {
+                                        isUp = true;
+                                        if (listView.getMeasuredHeight() < displayMetrics.heightPixels) {
+                                            setHeightAnimation(listView.getMeasuredHeight(), displayMetrics.heightPixels);
+                                        }
+                                        //rootLayout.animate().setDuration(300).alpha(1.0f);
+                                        //linearLayoutMove.animate().setDuration(300).translationY(listView.getHeight() - 50);
+                                    } else if (isUp) {
+                                        isUp = false;
+                                        setHeightAnimation(listView.getMeasuredHeight(), -displayMetrics.heightPixels);
+                                        //listView.animate().setDuration(300).translationY(displayMetrics.heightPixels - (displayMetrics.heightPixels - position));//(displayMetrics.heightPixels * 80) / 100
+                                        //rootLayout.animate().setDuration(300).alpha(0.0f);
+                                    }
+
+                                    break;
+
+                                case MotionEvent.ACTION_MOVE:
+                                    if (listView.getFirstVisiblePosition() == 0) {
+                                        ViewGroup.LayoutParams params = listView.getLayoutParams();
+                                        params.height = (int) (displayMetrics.heightPixels - motionEvent.getRawY());
+                                        listView.setLayoutParams(params);
+
+                                    } else {
+                                        return false;
+                                    }
+
+                                    break;
+                            }
+
+                            return true;
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+
+            }
+        });
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
 
         switch (motionEvent.getActionMasked()) {
-//
             case MotionEvent.ACTION_UP:
-                dX = (int) view.getX() - motionEvent.getRawX();
-                dY = (int) view.getY() - motionEvent.getRawY();
-                lastAction = MotionEvent.ACTION_DOWN;
 
-                if (!isUp){
+                if (!isUp) {
                     isUp = true;
-                    linearLayoutMove.animate().setDuration(300).translationY(0);
-                    rootLayout.animate().setDuration(300).alpha(1.0f);
+                    if (listView.getMeasuredHeight() < displayMetrics.heightPixels) {
+                        setHeightAnimation(listView.getMeasuredHeight(), displayMetrics.heightPixels);
+                    }
+                    //rootLayout.animate().setDuration(300).alpha(1.0f);
                     //linearLayoutMove.animate().setDuration(300).translationY(listView.getHeight() - 50);
-                }else if (isUp){
+                } else if (isUp) {
                     isUp = false;
-                    linearLayoutMove.animate().setDuration(300).translationY(displayMetrics.heightPixels - (displayMetrics.heightPixels - position));//(displayMetrics.heightPixels * 80) / 100
-                    rootLayout.animate().setDuration(300).alpha(0.0f);
+                    setHeightAnimation(listView.getMeasuredHeight(), -displayMetrics.heightPixels);
+                    //listView.animate().setDuration(300).translationY(displayMetrics.heightPixels - (displayMetrics.heightPixels - position));//(displayMetrics.heightPixels * 80) / 100
+                    //rootLayout.animate().setDuration(300).alpha(0.0f);
                 }
 
-                //linearLayoutMove.setY(displayMetrics.heightPixels / 4);
                 break;
 
             case MotionEvent.ACTION_MOVE:
-                String s = String.valueOf((int) view.getY());
-                Log.d("Eixo Y", s);
-//                if (position >= 0)
-//                    linearLayoutMove.setTranslationY(position);
-                break;
+                if (listView.getFirstVisiblePosition() != 0) {
+                    ViewGroup.LayoutParams params = listView.getLayoutParams();
+                    params.height = (int) (displayMetrics.heightPixels - motionEvent.getRawY());
+                    listView.setLayoutParams(params);
 
-//            case MotionEvent.ACTION_UP:
-//                if (view.getX() > (defaultPosition + defaultPosition)) {
-//                    view.setX(motionEvent.getRawX() * (defaultPosition));
-//                } else if (view.getX() < (defaultPosition - defaultPosition)) {
-//                    view.setX(motionEvent.getRawX() * (-defaultPosition));
-//                } else {
-//                    view.setX(defaultPosition);
-//                }
+                } else  {
+                    return false;
+                }
+
+                break;
         }
 
         return true;
+    }
+
+
+    private void setHeightAnimation(int fromHeight, int toHeight) {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(fromHeight, toHeight);
+        valueAnimator.setDuration(400);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int val = (Integer) animation.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = listView.getLayoutParams();
+                layoutParams.height = val;
+                listView.setLayoutParams(layoutParams);
+            }
+        });
+        valueAnimator.start();
     }
 
     private int dpToPx(int dp) {
